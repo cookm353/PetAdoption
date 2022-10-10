20cc3dc614b8a8b3b4f0481c39c82822227ebb25
Matthew Cook <cookm353@gmail.com>
2022-10-10 15:31:40 -0400

    added functions for getting pet(s) and adding

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ def connect_db(app):
     db.init_app(app)
     
 class Pet(db.Model):
+    """Class representing the pet table"""
     __tablename__ = 'pets'
     
     id = db.Column(db.Integer, primary_key=True, 
@@ -30,16 +31,54 @@ class Pet(db.Model):
         return f"<Pet name={self.name} species={self.species} available={self.available}>"
     
     def get_pets():
+        """Get all pets"""
         return Pet.query.all()
         
     def get_pet(id):
+        """Get a single pet"""
         return Pet.query.get_or_404(id)
-    
-    def add_pet(details):
-        ...
+        
+    def add_pet(pet_details):
+        """Create a pet and add to DB"""
+        pet = Pet(name=pet_details.get('name'), species=pet_details.get('species'),
+                  photo_url=pet_details.get('photo_url'), age=pet_details.get('age'),
+                  notes=pet_details.get('notes'), available=pet_details.get('isAvailable'))
+        
+        db.session.add(pet)
+        db.session.commit()
         
     def edit_pet(detail):
+        """Edit pet and changes"""
         ...
         
     def delete_pet(id):
         ...
+        
+    @property
+    def display_details(self):
+        """
+        Property for easily displaying pet's details
+        """
+        details = [self.name, self.species]
+        if self.photo_url:
+            details.append(self.photo_url)
+        if self.age:
+            details.append(self.age)
+            
+        return details
+        
+    @property
+    def edit_details(self):
+        """
+        Property for easily editing pet's details
+        """
+        details = []
+        
+        if self.photo_url:
+            details.append(self.photo_url)
+        if self.notes:
+            details.append(self.notes)
+            
+        details.append(self.available)
+        
+        return details
