47d16444dea49953f95abfa42f650d95ef6ba9eb
Matthew Cook <cookm353@gmail.com>
2022-10-11 10:16:56 -0400

    implemented tests for methods in model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-
+from werkzeug.exceptions import NotFound
 from app import app
 from models import db, Pet
 
@@ -24,3 +24,34 @@ class Test_Pet(TestCase):
         
     def tearDown(self):
         db.session.rollback()
+        
+    def test_get_pet(self):
+        """Test getting an individual pet"""
+        pet = Pet.get_pet(1)
+        self.assertEqual(pet.name, 'Lucy')
+        
+    def test_get_pet_404(self):
+        """Make sure trying to get a non-existent pet raises a 404"""
+        with self.assertRaises(NotFound):
+            Pet.get_pet(2)
+            
+    def test_get_all_pets(self):
+        """Test that we can retrieve all pets in DB"""
+        pets = Pet.get_pets()
+        lucy = pets[0]
+        
+        self.assertEqual(lucy.name, 'Lucy')
+        
+    def test_adding_pet(self):
+        """Make sure we can add a pet"""
+        Pet.add_pet({'name':'Buster', 'species': 'Dog', 'available': False})
+        
+        buster = Pet.get_pet(2)
+        self.assertEqual(buster.name, 'Buster')
+        
+    def test_editting_pet(self):
+        """Make sure we can edit a pet"""
+        lucy = Pet.get_pet(1)
+        Pet.edit_pet(lucy, {'available': True})
+        
+        self.assertEqual(lucy.available, True)
